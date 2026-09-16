654ae4f11d2cce12cdbe54e1a7088b20426da2fe
rorrop24 <ro.catalani@duocuc.cl>
2026-09-15 23:47:09 -0300

    subo el proyecto

---
+++ src/test/java/com/Pedidos360/Pedidos360/Pedidos360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pedidos360.Pedidos360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pedidos360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
